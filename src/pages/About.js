69a03fa1d0c90f1ed1b30d162f9f78e97b746d0f
MrNele <nemanja1225@gmail.com>
2020-07-22 17:56:38 -0700

    Add files via upload

---
+++ src/pages/About.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function About() {
+  return (
+  <section className="section about-section">
+    <h1 className="section-title">about us</h1>
+    <p>We have the best cocktail recipes all over the world! Try it :)</p>
+  </section>
+  )
+}
